c91b4f833fe22fa5bb07b051494df3f44c8f2d7a
pnadal <polnadalserra@gmail.com>
2025-06-14 21:06:59 +0200

    Initial Commit

---
+++ Printer/PrinterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrinterApp.swift
+//  Printer
+//
+//  Created by Pol Nadal Serra on 14/6/25.
+//
+
+import SwiftUI
+
+@main
+struct PrinterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
